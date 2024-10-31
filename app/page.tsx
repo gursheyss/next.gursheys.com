e9d3792ef88820bbf919def6eb5d67ade1e2b1a6
gursheyss <rkwarya@gmail.com>
2024-10-31 13:19:34 -0700

    add links

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import About from "@/components/About";
-
+import Links from "@/components/Links";
 export default function Home() {
   return (
     <main className="relative min-h-screen font-mono mx-auto flex max-w-7xl flex-col justify-center px-2 pt-4 sm:pt-0 lg:flex-row lg:px-0 z-aboveVignette">
       <div className="w-full lg:w-2/3">
         <About />
       </div>
-      <div className="w-full lg:w-1/3"></div>
+      <div className="w-full lg:w-1/3">
+        <Links />
+      </div>
     </main>
   );
 }
---
+++ components/Links.tsx
@@ -0,0 +1,40 @@
+export default function Links() {
+  return (
+    <div className="cutout-title border-box" data-title="⭐ links">
+      <div className="content">
+        <ul>
+          <li>
+            <a
+              href="https://www.github.com/gursheyss"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-pink hover:underline"
+            >
+              github
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/gurshan-warya/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-pink hover:underline"
+            >
+              linkedin
+            </a>
+          </li>
+          <li>
+            <a
+              href="mailto:gurshanwarya@gmail.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-pink hover:underline"
+            >
+              email
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
