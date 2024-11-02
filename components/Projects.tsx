87da0da247f7bb183b3cad9bb69d1f2a978b1847
gursheyss <rkwarya@gmail.com>
2024-11-02 02:52:17 -0700

    cut down sum text

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,99 +1,80 @@
+const PROJECTS_CONTENT = {
+  intro: "here's a couple of things i've worked on",
+  projects: [
+    {
+      title: "📈 streamerstocks",
+      tech: "sveltekit, supabase, python",
+      link: {
+        text: "streamerstocks",
+        url: "https://streamerstocks.org",
+      },
+      description: [
+        "is a simulated stock market based on twitch streamer jasontheween's community",
+      ],
+    },
+    {
+      title: "💊 meditag",
+      tech: "sveltekit, openai, supabase",
+      link: {
+        text: "meditag",
+        url: "https://github.com/gursheyss/MediTag",
+      },
+      description: [
+        "is a web app that lets nurses access and interact with patient data",
+      ],
+    },
+    {
+      title: "📂 zipp",
+      tech: "sveltekit, golang, mysql, s3",
+      link: {
+        text: "zipp",
+        url: "/zipp",
+      },
+      description: ["is an encrypted file sharing service"],
+    },
+    {
+      title: "🎵 austere",
+      tech: "golang, docker",
+      link: {
+        text: "austere",
+        url: "https://github.com/gursheyss/austere",
+      },
+      description: ["is a web server i use to save and tag unre"],
+    },
+  ],
+};
+
 const Projects = () => {
   return (
     <div className="border-box cutout-title" data-title="🛠️ projects">
       <div className="content">
-        here&apos;s a couple of things i&apos;ve worked on
-        <div
-          className="border-box cutout-title right-title"
-          data-title="📈 streamerstocks"
-          data-right-title="sveltekit, supabase, python"
-        >
-          <div className="content">
-            <ul>
-              <li>
-                »{" "}
-                <a
-                  href="https://streamerstocks.org"
-                  target="_blank"
-                  className="text-pink hover:underline"
-                >
-                  streamerstocks
-                </a>{" "}
-                is a simulated stock market based on twitch streamer
-                jasontheween&apos;s community that had over 3,500 users
-              </li>
-              <li>
-                » we used supabase + websockets for realtime updates and python
-                for the price simulation
-              </li>
-            </ul>
+        {PROJECTS_CONTENT.intro}
+        {PROJECTS_CONTENT.projects.map((project) => (
+          <div
+            key={project.title}
+            className="border-box cutout-title right-title"
+            data-title={project.title}
+            data-right-title={project.tech}
+          >
+            <div className="content">
+              <ul>
+                {project.description.map((desc, index) => (
+                  <li key={index}>
+                    »{" "}
+                    <a
+                      href={project.link.url}
+                      target="_blank"
+                      className="text-pink hover:underline"
+                    >
+                      {project.link.text}
+                    </a>{" "}
+                    {desc}
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
-        </div>
-        <div
-          className="border-box cutout-title right-title"
-          data-title="💊 meditag"
-          data-right-title="sveltekit, openai, supabase"
-        >
-          <div className="content">
-            <ul>
-              <li>
-                »{" "}
-                <a
-                  href="https://github.com/gursheyss/MediTag"
-                  target="_blank"
-                  className="text-pink hover:underline"
-                >
-                  meditag
-                </a>{" "}
-                is a web app that lets nurses access and interact with patient
-                data
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div
-          className="border-box cutout-title right-title"
-          data-title="📂 zipp"
-          data-right-title="sveltekit, golang, mysql, s3"
-        >
-          <div className="content">
-            <ul>
-              <li>
-                »{" "}
-                <a
-                  href="/zipp"
-                  target="_blank"
-                  className="text-pink hover:underline"
-                >
-                  zipp
-                </a>{" "}
-                is an encrypted file sharing service that implements aes-256 and
-                pbkdf2 encryption
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div
-          className="border-box cutout-title right-title"
-          data-title="🎵 austere"
-          data-right-title="golang, docker"
-        >
-          <div className="content">
-            <ul>
-              <li>
-                »{" "}
-                <a
-                  href="https://github.com/gursheyss/austere"
-                  target="_blank"
-                  className="text-pink hover:underline"
-                >
-                  austere
-                </a>{" "}
-                is a web server that&apos;s used to save and tag songs
-              </li>
-            </ul>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
