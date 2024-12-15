99cdbc1d7d0a00543c4f92cf8218aa7cda3408c1
gursheyss <rkwarya@gmail.com>
2024-12-15 11:33:16 -0800

    update work

--- components/Work.tsx
+++ components/Work.tsx
@@ -1,22 +1,27 @@
 const WORK_CONTENT = {
   links: {
+    fInc: {
+      text: 'f.inc',
+      url: 'https://f.inc'
+    },
     clipStudio: {
-      text: 'clip studio',
+      text: 'clip.studio',
       url: 'https://clip.studio/'
     },
     sce: {
       text: 'sce',
       url: 'https://sce.sjsu.edu/'
     },
-    mobiusEngine: {
-      text: 'mobius engine',
-      url: 'https://search.mobiusengine.ai'
-    }
+    // mobiusEngine: {
+    //   text: 'mobius engine',
+    //   url: 'https://search.mobiusengine.ai'
+    // }
   },
   items: [
-    "i'm working on an ai {clipStudio} video generation app",
+    "i'm currently working on a studio project at {fInc} for seo/ads",
+    "i'm also working on an ai video generation platform {clipStudio}",
     "i'm a dev officer and mentor at {sce}, a software engineering club at sjsu",
-    'i also worked on {mobiusEngine}, a job search engine'
+    // 'i also worked on {mobiusEngine}, a job search engine'
   ]
 }
 
@@ -26,15 +31,26 @@ const Work = () => {
       <div className="content">
         <ul>
           <li>
-            » currently i&apos;m working on an ai{' '}
+            » i&apos;m working on a studio project at{' '}
+            <a
+              href={WORK_CONTENT.links.fInc.url}
+              className="text-pink hover:underline"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {WORK_CONTENT.links.fInc.text}
+            </a>
+          </li>
+          <li>
+            » i&apos;m also working on an ai video generation platform called{' '}
             <a
               href={WORK_CONTENT.links.clipStudio.url}
               className="text-pink hover:underline"
               target="_blank"
+              rel="noreferrer"
             >
               {WORK_CONTENT.links.clipStudio.text}
-            </a>{' '}
-            video generation app
+            </a>
           </li>
           <li>
             » i&apos;m a dev officer and mentor at{' '}
@@ -42,22 +58,24 @@ const Work = () => {
               href={WORK_CONTENT.links.sce.url}
               className="text-pink hover:underline"
               target="_blank"
+              rel="noreferrer"
             >
               {WORK_CONTENT.links.sce.text}
             </a>
             , a software engineering club at sjsu
           </li>
-          <li>
+          {/* <li>
             » i also worked on{' '}
             <a
               href={WORK_CONTENT.links.mobiusEngine.url}
               className="text-pink hover:underline"
               target="_blank"
+              rel="noreferrer"
             >
               {WORK_CONTENT.links.mobiusEngine.text}
             </a>
             , a job search engine
-          </li>
+          </li> */}
         </ul>
       </div>
     </div>
