eb913c5ab56ac0b2bb3c1ff6be21d4fa7859d9d5
gursheyss <rkwarya@gmail.com>
2025-01-02 14:48:19 -0800

    change z index of snow

--- components/SnowfallEffect.tsx
+++ components/SnowfallEffect.tsx
@@ -4,15 +4,15 @@ import Snowfall from 'react-snowfall'
 
 export const SnowfallEffect = () => {
   return (
-    <Snowfall 
+    <Snowfall
       color="rgba(255, 255, 255, 0.5)"
       snowflakeCount={200}
       style={{
         position: 'fixed',
         width: '100vw',
         height: '100vh',
-        zIndex: 45
+        zIndex: -10
       }}
     />
   )
-} 
+}
