67a3509c19a69199efc7e0f08e6b63485228107c
gursheyss <rkwarya@gmail.com>
2025-01-02 14:47:36 -0800

    update number to use numberflow

--- components/Age.tsx
+++ components/Age.tsx
@@ -1,28 +1,25 @@
 'use client'
 import { useEffect, useState } from 'react'
+import NumberFlow from '@number-flow/react'
 
 export default function Age() {
-  const [age, setAge] = useState('0')
+  const [age, setAge] = useState(0)
 
   useEffect(() => {
     const birthday = new Date(2004, 10, 30)
-
-    const getAge = () => {
-      const now = new Date()
-      const diff = now.getTime() - birthday.getTime()
-      const age = diff / (1000 * 60 * 60 * 24 * 365.25)
-      return age.toFixed(9)
-    }
-
-    const updateAge = () => {
-      setAge(getAge())
-      requestAnimationFrame(updateAge)
-    }
-
-    updateAge()
-
-    return () => cancelAnimationFrame(updateAge as unknown as number)
+    const now = new Date()
+    const diff = now.getTime() - birthday.getTime()
+    setAge(diff / (1000 * 60 * 60 * 24 * 365.25))
   }, [])
 
-  return <span>{age}</span>
+  return (
+    <NumberFlow
+      value={age}
+      format={{
+        minimumFractionDigits: 8,
+        maximumFractionDigits: 8
+      }}
+      continuous
+    />
+  )
 }
