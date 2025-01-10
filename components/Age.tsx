abab4364ac698f6eb9e1bbc1eae29196220b94ca
gursheyss <rkwarya@gmail.com>
2025-01-10 06:53:22 -0800

    fix number update

--- components/Age.tsx
+++ components/Age.tsx
@@ -7,9 +7,17 @@ export default function Age() {
 
   useEffect(() => {
     const birthday = new Date(2004, 10, 30)
-    const now = new Date()
-    const diff = now.getTime() - birthday.getTime()
-    setAge(diff / (1000 * 60 * 60 * 24 * 365.25))
+
+    const calculateAge = () => {
+      const now = new Date()
+      const diff = now.getTime() - birthday.getTime()
+      setAge(diff / (1000 * 60 * 60 * 24 * 365.25))
+    }
+
+    calculateAge()
+    const interval = setInterval(calculateAge, 50)
+
+    return () => clearInterval(interval)
   }, [])
 
   return (
